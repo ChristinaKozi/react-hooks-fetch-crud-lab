9ada07869f2db560b7849378b0584ec4f9d448df
Christina Kozanian <christinakozanian@gmail.com>
2023-11-10 18:17:34 -0800

    lab complete

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -10,6 +10,7 @@ function QuestionForm(props) {
     correctIndex: 0,
   });
 
+  
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -19,7 +20,20 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const body ={
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex
+    }
+    fetch(`http://localhost:4000/questions`,{
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+      .then(r=>r.json())
+      .then(data=>data)
   }
 
   return (
